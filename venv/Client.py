7950f989b07bbf5b2b0ff1b28d667d63cc3010e2
Luke <luke.dawe@gmail.com>
2020-12-06 12:38:46 +0000

    Added Client class

--- venv/Client.py
+++ venv/Client.py
@@ -1,13 +1,17 @@
 import re
 import socket
 from typing import Dict
-
-Socket = socket.socket
 import Channel
 
+Socket = socket.socket
 
 class Client:
     def __init__(self, server: "Server", socket: Socket) -> None:
         self.server = server
         self.socket = socket
         self.channels: Dict[bytes, Channel] = {}
+        self.user = b""
+        if self.server.ipv6:
+            host, port, _, _ = socket.getpeername()
+        else:
+            host, port = socket.getpeername()
