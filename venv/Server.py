23934a76539ca0e6cb2a44de8d365ecdae21e258
Luke <luke.dawe@gmail.com>
2020-12-06 12:10:52 +0000

    Starting to make the connection with a server

--- venv/Server.py
+++ venv/Server.py
@@ -10,7 +10,16 @@ import time
 
 
 class Server:
-    def __init__(self, ports, password, channel, ipv6):
+    def __init__(self, ports, password, channel, ipv6, listen):
         self.ports = ports
         self.password = password
         self.ipv6 = ipv6
+
+        if self.ipv6 and listen:
+            self.address = socket.getaddrinfo(listen, None, proto=socket.IPPROTO_TCP)[0][4][0]
+        elif listen:
+            self.address = socket.gethostbyname(listen)
+        else:
+            self.address = ""
+            server_name_limit = 63  # From the RFC.
+            self.name = socket.getfqdn(self.address)[:server_name_limit].encode()
