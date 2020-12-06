de6905ce76696723ae3e0fe011432397f939e7b8
Luke <luke.dawe@gmail.com>
2020-12-06 12:16:44 +0000

    Accepting command line arguments.

--- venv/Server.py
+++ venv/Server.py
@@ -7,18 +7,18 @@ import string
 import sys
 import tempfile
 import time
-
+from argparse import ArgumentParser, Namespace
 
 class Server:
-    def __init__(self, ports, password, channel, ipv6, listen):
+    def __init__(self, args: Namespace, ports, password, channel, ipv6) -> None:
         self.ports = ports
         self.password = password
         self.ipv6 = ipv6
 
-        if self.ipv6 and listen:
-            self.address = socket.getaddrinfo(listen, None, proto=socket.IPPROTO_TCP)[0][4][0]
-        elif listen:
-            self.address = socket.gethostbyname(listen)
+        if self.ipv6 and args.listen:
+            self.address = socket.getaddrinfo(args.listen, None, proto=socket.IPPROTO_TCP)[0][4][0]
+        elif args.listen:
+            self.address = socket.gethostbyname(args.listen)
         else:
             self.address = ""
             server_name_limit = 63  # From the RFC.
