e3a2dbf6558fbabdfb209fa9ded568dfadd6e5e2
Luke <luke.dawe@gmail.com>
2020-12-09 20:48:38 +0000

    Starting to let the server have several threads with channels on them. Dealt with the error if no IPv6 was set.

--- venv/Channel.py
+++ venv/Channel.py
@@ -1,12 +1,14 @@
+import threading
 from typing import Any, Collection, Dict, List, Optional, Sequence, Set
 import Server
-
+from Client import Client
 
 class Channel:
     def __init__(self, server: "Server", name: bytes) -> None:
         self.server = server
         self.name = name
         self.members: Set["Client"] = set()
+        self.thread_id = threading.get_ident()
 
     def add_member(self, client: "Client") -> None:
         self.members.add(client)
