b2c632c0fc7317e23815a75f488d3d3a561ae3df
Jesse <jessechan10@gmail.com>
2020-12-18 19:53:48 +0000

    Tidy up the code

--- bot.py
+++ bot.py
@@ -9,17 +9,16 @@ server = "chat.freenode.net"
 channel = "##testchanneloneagz"
 botnick = "Ginger"
 
-
+# Create a socket instance. This facilitates a bots connection to the server
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # This function will take a random line from the fact.txt file and return it.
 # It is required for the '!fact' command
+
+
 def random_line(fname):
     line = random.choice(open(fname, 'r', encoding='cp850').readlines())
     return line
-
-
-# Create a socket instance. This facilitates a bots connection to the server
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 # Connects the bot to the server and checks for errors in case of failure.
@@ -39,6 +38,7 @@ def connect_to_server():
 
     print(botnick + " is here")
 
+
 def get_names():
     irc.send(bytes('NAMES ' + channel + '\r\n', "UTF-8"))
     time.sleep(1)
@@ -46,40 +46,44 @@ def get_names():
     return getnamelist.split(channel, 1)[1].split(':', 1)[1].split('\r\n', 1)[0].split(' ')
 
 
+def get_time():
+    current_local_time = time.localtime()
+    current_time = time.strftime("%H:%M:%S", current_local_time)
+    return current_time
+
+
+def ping(append_to_ping):
+    irc.send(bytes('PONG ' + append_to_ping + '\r\n', "UTF-8"))
 # Main method
+
+
 def main():
     connect_to_server()
+    time.sleep(1)
     # This code will run continuously
-    ran = False
-
     while 1:
         # Constantly try to read information in from the socket
-
         try:
             text = irc.recv(2048).decode("UTF-8")
             print(text)
         except Exception:
             pass
-        t0 = time.time()
         # If someone sends a message in the channel then take time (for !hello), get name of senders / relevant channel
         if text.find("PRIVMSG") != -1:
-            t = time.localtime()
-            current_time = time.strftime("%H:%M:%S", t)
+            localtime = get_time()
             name = text.split('!', 1)[0][1:]
             chat = text.split('PRIVMSG', 1)[1].split(' ', 1)[1].split(' ', 1)[0]
 
             # PING/PONG to/from the server to check for timeouts
             # Takes second element on Ping appends it to pong so it is correct to server
             if text.find('PING') != -1:
-                irc.send(bytes('PONG ' + text.split()[1] + '\r\n', "UTF-8"))
+                ping(text.split()[1])
 
             # Hello command - gives time
             if text.find(":!hello") != -1:
-                irc.send(bytes("PRIVMSG " + chat + " :Hello, the time is " + current_time + "!\r\n", 'UTF-8'))
+                irc.send(bytes("PRIVMSG " + chat + " :Hello, the time is " + localtime + "!\r\n", 'UTF-8'))
 
-            # Slap command:
             # Use 'NAMES' command to get names of all channel users. Sleep to cope with user spam
-            # Use what is returned by NAMES to create a list of active users on the channel (listOfNames)
             # From this list, choose a random user and designate them the 'slapee'. Then slap them
             if text.find(":!slap") != -1:
                 slapee = random.choice(get_names())
@@ -90,7 +94,6 @@ def main():
 
                 irc.send(bytes("PRIVMSG " + chat + " :Slaps " + slapee + " around with a wet trout\r\n", 'UTF-8'))
 
-            # Fact command:
             # If the user does '!fact' in the public channel, a fact will be private messaged to them.
             # If the user sends a private message to the bot, the bot responds with a fact
             if text.find(":!fact") != -1:
@@ -99,16 +102,9 @@ def main():
             elif chat == botnick:
                 irc.send(bytes("PRIVMSG " + name + " :" + random_line("facts.txt") + "\r\n", 'UTF-8'))
 
-            if ran == 1:
-                if len(get_names()) == 1:
-                    irc.send(bytes("PRIVMSG " + chat + " :" + "So lonely and sad " + "\r\n", 'UTF-8'))
-                else:
-                    irc.send(bytes("PRIVMSG " + chat + " :" + "Ok i'm just going to leave because no one is speaking " + "\r\n", 'UTF-8'))
-
-            ran = True
 
 # This run the main method
 if __name__ == "__main__":
     main()
 
-# Jesse wants this ---> message = text.split('PRIVMSG', 1)[1].split(':', 1)[1]
+
