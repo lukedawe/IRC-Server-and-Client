7ac63faa43b5907202a672dee0b49fd56f64011e
Luke <luke.dawe@gmail.com>
2020-12-18 16:17:04 +0000

    Moved methods into Server.py

--- venv/Channel.py
+++ venv/Channel.py
@@ -12,7 +12,7 @@ class Channel:
     # https://github.com/jrosdahl/miniircd/blob/master/miniircd line 47
     threadID = None
 
-    def __init__(self, server: "Server", name: bytes, serverSocket) -> None:
+    def __init__(self, server: "Server", name: str, serverSocket) -> None:
         self.server = server
         self.name = name
         self.members = {}
@@ -33,109 +33,3 @@ class Channel:
         # Remove from our list of users
         del self.clientList[client]
 
-    def receiveMessage(self, clientSocket):
-        try:
-            messageHeader = clientSocket.recv(HEADER_LENGTH)
-            print(messageHeader)
-            if not len(messageHeader):
-                return False
-
-            messageLength = int(messageHeader.decode('utf-8').strip())
-            return {'header': messageHeader, 'msgData': clientSocket.recv(messageLength)}
-
-        except:
-            return False
-
-    def refreshChannel(self):
-        while True:
-            read_sockets, _, exception_sockets = select.select(self.socketList, [], self.socketList)
-
-            # Iterate over notified sockets
-            for notified_socket in read_sockets:
-
-                # If notified socket is a server socket - new connection, accept it
-                if notified_socket == self.serverSocket:
-
-                    # Accept new connection
-                    # That gives us new socket - client socket, connected to this given client only, it's unique for that client
-                    # The other returned object is ip/port set
-                    client_socket, client_address = self.serverSocket.accept()
-
-                    # Client should send his name right away, receive it
-                    user = self.receiveMessage(client_socket)
-                    if not user:
-                        continue
-                    print(user['msgData'].decode('utf-8'))
-
-                    # If False - client disconnected before he sent his name
-                    if user is False:
-                        continue
-
-                    # Add accepted socket to select.select() list
-                    self.socketList.append(client_socket)
-
-                    # Also save username and username header
-                    self.clientList[client_socket] = user
-
-                    address = str(client_address[0]) + ":" + str(client_address[1])
-
-                    # And add member
-                    self.addMember(client_socket, address)
-
-                    print('Accepted new connection from {} username: {}'.format(address, user['msgData'].decode('utf-8')))
-
-                    textToSend = ":" + user['msgData'].decode("utf-8") + "!~" + user['msgData'].decode("utf-8") + "@" + self.members[client_socket] + " JOIN " + str(self.name)
-                    print(f'Sent Text: {textToSend}')
-
-                    textToSend = textToSend.encode()
-                    client_socket.send(textToSend)
-
-                # Else existing socket is sending a message
-                else:
-
-                    # Receive message
-                    message = self.receiveMessage(notified_socket)
-
-                    # If False, client disconnected, cleanup
-                    if message is False:
-                        # Remove from list for socket.socket()
-                        self.removeClient(notified_socket)
-
-                        continue
-
-                    # Get user by notified socket, so we will know who sent the message
-                    user = self.clientList[notified_socket]
-
-                    print(f'Received message from {user["msgData"].decode("utf-8")}: {message["msgData"].decode("utf-8")}')
-
-                    # Iterate over connected clients and broadcast message
-                    for client_socket in self.clientList:
-
-                        # But don't sent it to sender
-                        if client_socket != notified_socket:
-                            # Send user and message (both with their headers)
-                            # We are reusing here message header sent by sender, and saved username header send by user when he connected
-                            # client_socket.send(user['header'] + user['msgData'] + message['header'] + message['msgData'])
-                            textToSend = ":" + user['msgData'].decode("utf-8") + "!" + user['msgData'].decode("utf-8") + "@" + self.members[client_socket] + " PRIVMSG " + str(self.name) + " :" + message['msgData'].decode("utf-8") # NEED TO ENCODE AGAIN TO SEND
-                            print(f'Sent Text: {textToSend}')
-                            textToSend = textToSend.encode()
-                            client_socket.send(textToSend)
-
-            # It's not really necessary to have this, but will handle some socket exceptions just in case
-            for notified_socket in exception_sockets:
-                # Remove from list for socket.socket()
-                self.socketList.removeClient(notified_socket)
-
-                # Remove from our list of users
-                del self.clientList[notified_socket]
-
-
-# from https://github.com/jrosdahl/miniircd/blob/master/miniircd lines 1053-1060
-_ircstring_translation = bytes.maketrans(
-    (string.ascii_lowercase.upper() + "[]\\^").encode(),
-    (string.ascii_lowercase + "{}|~").encode(),
-)
-
-
-def irc_lower(s: bytes) -> bytes:
-    return s.translate(_ircstring_translation)
--- venv/Server.py
+++ venv/Server.py
@@ -10,13 +10,15 @@ import select
 # import irc
 
 Socket = socket.socket
+MSGLEN = 2048
 
 
 class Server:
     # https://github.com/jrosdahl/miniircd/blob/master/miniircd line 789
     def __init__(self, ports=None, password="", channel="test", ipv6=ipaddress.ip_address('::1')) -> None:
+        self.members = {}
         if ports is None:
-            ports = [1234]  # default port for server
+            ports = [6667]  # default port for server
 
         self.ports = ports
         self.ipv6 = ipv6
@@ -49,6 +51,7 @@ class Server:
         self.clients: Dict[Socket, Client] = {}
         self.nicknames: Dict[bytes, Client] = {}  # key: irc_lower(nickname)
         # self.threads: Dict[bytes, Channel] = {}
+        self.clientList = {}
 
         self.initialiseServer()
 
@@ -64,14 +67,13 @@ class Server:
     def initialiseServer(self):
         # address = socket.getaddrinfo(str(self.ipv6), self.ports[0], proto=socket.IPPROTO_TCP)[0][4][0]
         # address = [addr for addr in socket.getaddrinfo(self.ipv6, None) if socket.AF_INET6 == addr[0]]
-        ipv4 = '127.0.0.1'
 
         self.serverSocket.bind((str(self.ipv6), self.ports[0]))
         self.serverSocket.listen()
 
         print(f'Listening for connections on {self.ipv6}:{self.ports[0]}...')
         channel1 = self.addChannel()
-        channel1.refreshChannel()
+        self.refreshServer()
         """
         for port in self.ports:
             s = socket.socket(
@@ -88,8 +90,145 @@ class Server:
         self.serverSocket.listen()
         """
 
-    def removeClient(self):
-        pass
+    # TODO find what channel the client wants to join
+    # TODO add that client to the list of channel members
+    def initReg(self) -> bool:
+        # Accept new connection
+        # That gives us new socket - client socket, connected to this given client only, it's unique for that client
+        # The other returned object is ip/port set
+        client_socket, client_address = self.serverSocket.accept()
 
-    def addClient(self, channel: Channel, client: Client):
-        Channel.addMember(client)
+        # Client should send his name right away, receive it
+        cap = self.reveiveMessageMk3(client_socket)
+        user = self.reveiveMessageMk3(client_socket)
+
+        if not user:
+            return False
+
+        # print("message data: " + user['msgData'].decode('utf-8'))
+
+        # Add accepted socket to select.select() list
+        self.socketList.append(client_socket)
+
+        # Also save username and username header
+        self.clientList[client_socket] = user
+
+        address = str(client_address[0]) + ":" + str(client_address[1])
+
+        # And add member
+        self.addMember(client_socket, address)
+
+        print('Accepted new connection from {} username: {}'.format(address,
+                                                                    user['msgData'].decode('utf-8')))
+
+        textToSend = ":" + user['msgData'].decode("utf-8") + "!~" + user['msgData'].decode(
+            "utf-8") + "@" + self.members[client_socket] + " JOIN " + self.name
+        print(f'Sent Text: {textToSend}')
+
+        textToSend = textToSend.encode()
+        client_socket.send(textToSend)
+
+    def receiveMessage(self, clientSocket):
+        try:
+            messageHeader = clientSocket.recv(10)
+            print("message header: " + messageHeader)
+            if not len(messageHeader):
+                return False
+
+            messageLength = int(messageHeader.decode('utf-8').strip())
+            return {'header': messageHeader, 'msgData': clientSocket.recv(messageLength)}
+
+        except:
+            return False
+
+    def receiveMessageMk2(self, clientSocket):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = clientSocket.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+    def reveiveMessageMk3(self, clientSocket):
+        chunk = clientSocket.recv(2048).decode("UTF-8")
+        return chunk
+
+    def refreshServer(self):
+        while True:
+            read_sockets, _, exception_sockets = select.select(self.socketList, [], self.socketList)
+
+            # Iterate over notified sockets
+            for notified_socket in read_sockets:
+
+                # If notified socket is a server socket - new connection, accept it
+                if notified_socket == self.serverSocket:
+                    if not self.initReg():
+                        continue
+
+                # Else existing socket is sending a message
+                else:
+
+                    # Receive message
+                    message = self.receiveMessageMk2(notified_socket)
+
+                    # If False, client disconnected, cleanup
+                    if message is False:
+                        # Remove from list for socket.socket()
+                        self.removeClient(notified_socket)
+
+                        continue
+
+                    # Get user by notified socket, so we will know who sent the message
+                    user = self.clientList[notified_socket]
+
+                    print(
+                        f'Received message from {user["msgData"].decode("utf-8")}: {message["msgData"].decode("utf-8")}')
+
+                    # Iterate over connected clients and broadcast message
+                    for client_socket in self.clientList:
+
+                        # But don't sent it to sender
+                        if client_socket != notified_socket:
+                            # Send user and message (both with their headers)
+                            # We are reusing here message header sent by sender, and saved username header send by user when he connected
+                            # client_socket.send(user['header'] + user['msgData'] + message['header'] + message['msgData'])
+                            textToSend = ":" + user['msgData'].decode("utf-8") + "!" + user['msgData'].decode(
+                                "utf-8") + "@" + self.members[client_socket] + " PRIVMSG " + str(self.name) + " :" + \
+                                         message['msgData'].decode("utf-8")  # NEED TO ENCODE AGAIN TO SEND
+                            print(f'Sent Text: {textToSend}')
+                            textToSend = textToSend.encode()
+                            client_socket.send(textToSend)
+
+            # It's not really necessary to have this, but will handle some socket exceptions just in case
+            for notified_socket in exception_sockets:
+                # Remove from list for socket.socket()
+                self.socketList.pop(notified_socket)
+
+                # Remove from our list of users
+                del self.clientList[notified_socket]
+
+    def addMember(self, client, clientaddress) -> None:
+        self.members[client] = clientaddress
+
+    def removeClient(self, client):
+        print('Closed connection from: {}'.format(self.clientList[client]['msgData'].decode('utf-8')))
+
+        # Remove from list for socket.socket()
+        self.socketList.remove(client)
+
+        # Remove from our list of users
+        del self.clientList[client]
+
+
+# from https://github.com/jrosdahl/miniircd/blob/master/miniircd lines 1053-1060
+_ircstring_translation = bytes.maketrans(
+    (string.ascii_lowercase.upper() + "[]\\^").encode(),
+    (string.ascii_lowercase + "{}|~").encode(),
+)
+
+
+def irc_lower(s: bytes) -> bytes:
+    return s.translate(_ircstring_translation)
