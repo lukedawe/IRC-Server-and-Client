a68cc9cb2c6a0ecd4a744a52c312cb78dfbcd170
Jesse <jessechan10@gmail.com>
2020-12-19 21:39:49 +0000

    Added Quit

--- bot.py
+++ bot.py
@@ -5,38 +5,32 @@ import random
 import time
 
 # Create basic variables to access server
-
 #channel = "#test"
-#server = "irc.ipv6.freenode.net"
-
+#server = "fe80::7f63:edf1:bc43:9bb2"
 server = "chat.freenode.net"
 channel = "##testchanneloneagz"
-
 
 botnick = "Ginger"
 
 # Create a socket instance. This facilitates a bots connection to the server
-
 #irc = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 # This function will take a random line from the fact.txt file and return it.
-# It is required for the '!fact' command
-
 def random_line(fname):
     try:
         line = random.choice(open(fname, 'r', encoding='cp850').readlines())
         return line
 
     except IOError:
-        return "File not found"
+        return "Facts file not found"
+
+
 # Connects the bot to the server and checks for errors in case of failure.
-
-
 def connect_to_server():
     try:
-        #irc.connect((server, 6667,0,0))
+        #irc.connect((server, 6667, 0, 0))
         irc.connect((server, 6667))
     except socket.error:
         print('Error connecting to IRC server')
@@ -53,10 +47,11 @@ def connect_to_server():
 
 
 def get_names():
+    time.sleep(0.2)
     irc.send(bytes('NAMES ' + channel + '\r\n', "UTF-8"))
     getnamelist = irc.recv(2048).decode("UTF-8")
+    print(getnamelist)
     namelist = getnamelist.split(channel, 1)[1].split(':', 1)[1].split('\r\n', 1)[0].split(' ')
-    print (namelist)
     return namelist
 
 
@@ -68,9 +63,9 @@ def get_time():
 
 def ping(append_to_ping):
     irc.send(bytes('PONG ' + append_to_ping + '\r\n', "UTF-8"))
+
+
 # Main method
-
-
 def main():
     connect_to_server()
     time.sleep(1)
@@ -82,6 +77,7 @@ def main():
             print(text)
         except Exception:
             pass
+
         # If someone sends a message in the channel then take time (for !hello), get name of senders / relevant channel
         if text.find("PRIVMSG") != -1:
             localtime = get_time()
@@ -95,7 +91,6 @@ def main():
                 ping(text.split()[1])
 
             # Hello command - gives time
-
             if message == '!hello\r\n':
                 irc.send(bytes("PRIVMSG " + chat + " :Hello, the time is " + localtime + "!\r\n", 'UTF-8'))
 
@@ -107,7 +102,6 @@ def main():
                 # Special case where bot chooses to slap itself
                 if slapee == botnick:
                     irc.send(bytes("PRIVMSG " + chat + " :Self harm is not a joke, but here goes...\r\n", 'UTF-8'))
-
                 irc.send(bytes("PRIVMSG " + chat + " :Slaps " + slapee + " around with a wet trout\r\n", 'UTF-8'))
 
             # If the user does '!fact' in the public channel, a fact will be private messaged to them.
@@ -118,13 +112,19 @@ def main():
             elif chat == botnick:
                 irc.send(bytes("PRIVMSG " + name + " :" + random_line("facts.txt") + "\r\n", 'UTF-8'))
 
+            # The '!user' command is here to explicitly show that our bot can keep track of users on the same channel
             if message == '!users\r\n':
-               test = get_names()
-               irc.send(bytes("PRIVMSG " + chat + " :Users are " +" "  .join(test) + "\r\n", 'UTF-8'))
+                test = get_names()
+                irc.send(bytes("PRIVMSG " + chat + " :Users are " + " ".join(test) + "\r\n", 'UTF-8'))
 
+        #If the someone leaves it will check how many people and in if it's one person it will leave
+        if text.find("QUIT") != -1:
+            if len(get_names()) == 1:
+                print("One boy left that means i leave ")
+                irc.send(bytes("QUIT Bye", "UTF-8"))
+                break
 
 # This run the main method
 if __name__ == "__main__":
     main()
-
 
